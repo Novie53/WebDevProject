7edfdf61b799c710c22b76a3a008022f123605eb
Novie53 <Novie53@users.noreply.github.com>
2023-10-11 11:29:28 +0200

    handlebars implementation part 1

--- Server.js
+++ Server.js
@@ -1,10 +1,19 @@
-const express = require('express')
+const express = require('express');
+const { engine } = require('express-handlebars');
 const fs = require("fs");
 
+const port = 80;
+const app = express();
 
-const app = express()
-app.use(express.static('public'))
-const port = 80
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.static('public'));
+app.use((req, res, next) => {
+    console.log("Req. URL: ", req.url);
+    next();
+});
 
 const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database('project.db')
@@ -12,26 +21,19 @@ const db = new sqlite3.Database('project.db')
 
 
 
+
 app.get('/', (req, res) => {
-    fs.readFile("main.html", (error, data) => {
-        res.status(200).type("html").send(data);
-    });
-})
-app.get('/products', (req, res) => {
-    fs.readFile("products.html", (error, data) => {
-        res.status(200).type("html").send(data);
-    });
-})
+    res.render('home');
+});
 app.get('/about', (req, res) => {
-    fs.readFile("about.html", (error, data) => {
-        res.status(200).type("html").send(data);
-    });
-})
+    res.render('about');
+});
 app.get('/contact', (req, res) => {
-    fs.readFile("contact.html", (error, data) => {
-        res.status(200).type("html").send(data);
-    });
-})
+    res.render('contact');
+});
+app.get('/products', (req, res) => {
+    res.render('products');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
