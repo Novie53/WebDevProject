9f61d486e82f4d4e9f89587974936883e72f5401
Novie53 <Novie53@users.noreply.github.com>
2023-10-11 13:26:23 +0200

    all products page and individual product pages

--- Server.js
+++ Server.js
@@ -22,6 +22,8 @@ const db = new sqlite3.Database('project.db')
 
 
 
+
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -32,8 +34,32 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 app.get('/products', (req, res) => {
-    res.render('products');
+    db.all("SELECT * FROM products", (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: 'Server Error'});
+        }
+        else {
+            const model = {productList: rows};
+            res.render('products', model);
+        }
+    });
 });
+app.get('/product/:id', function(req, res) {
+    const id = req.params.id;
+    db.get("SELECT * FROM products WHERE pID = ?", [id], (err, row) => {
+        if (err) {
+            res.status(500).json({ error: 'Server Error'});
+        }
+        else if (row) {
+            const model = row;
+            res.render("product", model);
+        }
+        else {
+            res.status(404).render('404');
+        }
+    });
+});
+
 app.use(function(req, res) {
     res.status(404).render('404');
 })
